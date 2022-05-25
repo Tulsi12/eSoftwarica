7a428b9c674b93027738f4cca47aee7a169f6430
Tulsi12 <katteltulsi9819@gmail.com>
2022-05-25 23:47:39 +0545

    entities creaated

---
+++ lib/entities/student_entity.dart
@@ -0,0 +1,18 @@
+import 'package:floor/floor.dart';
+
+@entity
+class Student {
+  @PrimaryKey(autoGenerate: true)
+  int? id;
+  String name;
+  String age;
+  String gender;
+  String address;
+
+  Student({
+    this.id,
+    required this.name,
+    required this.age, 
+    required this.gender, 
+    required this.address});
+}
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
@@ -11,13 +10,8 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        
-
-      ),
-      
+    return const Scaffold(
+      body: Center(),
     );
-    
   }
 }
